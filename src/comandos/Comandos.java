b84efea8f0e4350424782e409c503ee27ee0fea5
jl-manzano <jl.manzano@iesnervion.es>
2025-01-27 13:47:38 +0100

    Cambios en el código

--- src/comandos/Comandos.java
+++ src/comandos/Comandos.java
@@ -3,8 +3,8 @@ package comandos;
 public class Comandos {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hola mundo");
+
+		System.out.println("Adios mundo ~ jl.manzano");
 	}
 
 }
